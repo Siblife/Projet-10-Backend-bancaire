4e82b622de551b41455716bf65647c879d28b913
Matthieu <mbazeugeauddev@gmail.com>
2025-08-18 22:29:27 +0200

    début projet, front end

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import User from "./pages/User.jsx";
+import Login from "./pages/Login.jsx";
+import { Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
